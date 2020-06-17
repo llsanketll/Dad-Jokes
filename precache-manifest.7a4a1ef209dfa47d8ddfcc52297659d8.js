6c5262aaf5bfa7e65e2f9a962103b2c9f352fba7
llsanketll <sanket.lamsal@gmail.com>
2020-06-17 14:35:07 +0545

    Updates

---
+++ precache-manifest.7a4a1ef209dfa47d8ddfcc52297659d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0453e75c77b85df2e52902fc8a17c31b",
+    "url": "/Dad-Jokes/index.html"
+  },
+  {
+    "revision": "d4ace37d8ab5ceb701f9",
+    "url": "/Dad-Jokes/static/css/main.dbae5452.chunk.css"
+  },
+  {
+    "revision": "0346e008ceada25571ff",
+    "url": "/Dad-Jokes/static/js/2.30302c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dad-Jokes/static/js/2.30302c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ace37d8ab5ceb701f9",
+    "url": "/Dad-Jokes/static/js/main.8b84ed3d.chunk.js"
+  },
+  {
+    "revision": "bb5bfdc88884c825ff4e",
+    "url": "/Dad-Jokes/static/js/runtime-main.4f19fd0c.js"
+  }
+]);
